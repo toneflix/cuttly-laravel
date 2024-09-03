8e0b5a5050583060f04411b8483a6cf1bd554be3
3m1n3nc3 <mygames.ng@gmail.com>
2024-09-03 23:07:13 +0100

    Run tests without using the Facades.

--- tests/Feature/RegularTest.php
+++ tests/Feature/RegularTest.php
@@ -1,21 +1,25 @@
 <?php
 
-use ToneflixCode\Cuttly\Facades\Cuttly;
+use ToneflixCode\Cuttly\Cuttly;
 
 beforeEach(function () {
     $this->link = 'https://facebook.com/marxemi';
 }); //->skip('All tests in this file are temporarily disabled but they work');
 
 test('can shorten link', function () {
-    $create = Cuttly::regular()->noTitle()->public()->name('john'.rand())->shorten($this->link);
-    Cuttly::regular()->delete($create->shortLink);
+    $cuttly = new Cuttly();
+
+    $create = $cuttly->regular()->noTitle()->public()->name('john' . rand())->shorten($this->link);
+    $cuttly->regular()->delete($create->shortLink);
 
     expect($create->fullLink)->toBe($this->link);
 }); //->skip('temporarily disabled but it works.');
 
 test('can shorten link with minimal params', function () {
-    $create = Cuttly::regular()->shorten($this->link);
-    Cuttly::regular()->delete($create->shortLink);
+    $cuttly = new Cuttly();
+
+    $create = $cuttly->regular()->shorten($this->link);
+    $cuttly->regular()->delete($create->shortLink);
 
     expect($create->fullLink)->toBe($this->link);
 }); //->skip('temporarily disabled but it works.');
@@ -24,10 +28,12 @@ test('can shorten link with minimal params', function () {
  * Test if links can be edited
  */
 test('can edit link', function () {
-    $name = 'john'.rand();
-    $create = Cuttly::regular()->noTitle()->public()->name('john'.rand())->shorten($this->link);
-    $edit = Cuttly::regular()->noTitle()->name($name)->edit($create->shortLink);
-    Cuttly::regular()->delete(explode('john', $create->shortLink)[0].$name);
+    $name = 'john' . rand();
+    $cuttly = new Cuttly();
+
+    $create = $cuttly->regular()->noTitle()->public()->name('john' . rand())->shorten($this->link);
+    $edit = $cuttly->regular()->noTitle()->name($name)->edit($create->shortLink);
+    $cuttly->regular()->delete(explode('john', $create->shortLink)[0] . $name);
 
     expect($edit->status)->toBe(1);
 }); //->skip('temporarily disabled but it works.');
@@ -36,8 +42,10 @@ test('can edit link', function () {
  * Test if links can be deleted
  */
 test('can delete link', function () {
-    $create = Cuttly::regular()->noTitle()->public()->name('john'.rand())->shorten($this->link);
-    $delete = Cuttly::regular()->delete($create->shortLink);
+    $cuttly = new Cuttly();
+
+    $create = $cuttly->regular()->noTitle()->public()->name('john' . rand())->shorten($this->link);
+    $delete = $cuttly->regular()->delete($create->shortLink);
 
     expect($delete->status)->toBe(1);
 }); //->skip('temporarily disabled but it works.');
@@ -46,9 +54,11 @@ test('can delete link', function () {
  * Test if links stats can be aquired
  */
 test('can get stats', function () {
-    $create = Cuttly::regular()->noTitle()->public()->name('john'.rand())->shorten($this->link);
-    $stats = Cuttly::regular()->stats($create->shortLink);
-    Cuttly::regular()->delete($create->shortLink);
+    $cuttly = new Cuttly();
+
+    $create = $cuttly->regular()->noTitle()->public()->name('john' . rand())->shorten($this->link);
+    $stats = $cuttly->regular()->stats($create->shortLink);
+    $cuttly->regular()->delete($create->shortLink);
 
     expect($stats->fullLink)->toBe($this->link);
     expect($stats->shortLink)->toBe($create->shortLink);
--- tests/Feature/TeamTest.php
+++ tests/Feature/TeamTest.php
@@ -1,14 +1,16 @@
 <?php
 
-use ToneflixCode\Cuttly\Facades\Cuttly;
+use ToneflixCode\Cuttly\Cuttly;
 
 beforeEach(function () {
     $this->link = 'https://facebook.com/marxemi';
 }); //->skip('All tests in this file are temporarily disabled but they work');
 
 test('can shorten link', function () {
-    $create = Cuttly::team()->noTitle()->public()->name('john'.rand())->shorten($this->link);
-    Cuttly::team()->delete($create->shortLink);
+    $cuttly = new Cuttly();
+
+    $create = $cuttly->team()->noTitle()->public()->name('john' . rand())->shorten($this->link);
+    $cuttly->team()->delete($create->shortLink);
 
     expect($create->fullLink)->toBe($this->link);
 });
@@ -17,10 +19,12 @@ test('can shorten link', function () {
  * Test if links can be edited
  */
 test('can edit link', function () {
-    $name = 'john'.rand();
-    $create = Cuttly::team()->noTitle()->public()->name('john'.rand())->shorten($this->link);
-    $edit = Cuttly::team()->noTitle()->name($name)->edit($create->shortLink);
-    Cuttly::team()->delete(explode('john', $create->shortLink)[0].$name);
+    $name = 'john' . rand();
+    $cuttly = new Cuttly();
+
+    $create = $cuttly->team()->noTitle()->public()->name('john' . rand())->shorten($this->link);
+    $edit = $cuttly->team()->noTitle()->name($name)->edit($create->shortLink);
+    $cuttly->team()->delete(explode('john', $create->shortLink)[0] . $name);
 
     expect($edit->status)->toBe(1);
 }); //->skip('temporarily disabled but it works.');
@@ -29,8 +33,10 @@ test('can edit link', function () {
  * Test if links can be deleted
  */
 test('can delete link', function () {
-    $create = Cuttly::team()->noTitle()->public()->name('john'.rand())->shorten($this->link);
-    $delete = Cuttly::team()->delete($create->shortLink);
+    $cuttly = new Cuttly();
+
+    $create = $cuttly->team()->noTitle()->public()->name('john' . rand())->shorten($this->link);
+    $delete = $cuttly->team()->delete($create->shortLink);
 
     expect($delete->status)->toBe(1);
 }); //->skip('temporarily disabled but it works.');
@@ -39,9 +45,11 @@ test('can delete link', function () {
  * Test if links stats can be aquired
  */
 test('can get stats', function () {
-    $create = Cuttly::team()->noTitle()->public()->name('john'.rand())->shorten($this->link);
-    $stats = Cuttly::team()->stats($create->shortLink);
-    Cuttly::team()->delete($create->shortLink);
+    $cuttly = new Cuttly();
+
+    $create = $cuttly->team()->noTitle()->public()->name('john' . rand())->shorten($this->link);
+    $stats = $cuttly->team()->stats($create->shortLink);
+    $cuttly->team()->delete($create->shortLink);
 
     expect($stats->fullLink)->toBe($this->link);
     expect($stats->shortLink)->toBe($create->shortLink);
